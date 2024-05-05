3ae1bf025d9629a5dc4aee2f1a1473da3bbf1fed
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2024-05-05 07:19:50 +0000

    setup

--- server/app/controllers/posts_controller.rb
+++ server/app/controllers/posts_controller.rb
@@ -4,16 +4,15 @@ class PostsController < ApplicationController
   before_action :set_book, only: %i[show destroy]
 
   def show
-    post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.json { render json: post }
-    end
+    render :show
+    # respond_to do |format|
+    #   format.html { render :show }
+    #   format.json { render json: @post }
+    # end
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy
+    @post.destroy
 
     respond_to do |format|
       format.json { head :no_content }
